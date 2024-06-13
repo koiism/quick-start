844b87d83a1390279bce4740fd88806d92de30c0
pidanmeng <1092333914@qq.com>
2024-06-13 12:30:23 +0800

    feat: 🎸 switch renderer to skyline

--- apps/microapp/src/app.config.ts
+++ apps/microapp/src/app.config.ts
@@ -1,11 +1,19 @@
 export default defineAppConfig({
-  pages: [
-    'pages/index/index'
-  ],
+  pages: ['pages/index/index'],
   window: {
     backgroundTextStyle: 'light',
     navigationBarBackgroundColor: '#fff',
     navigationBarTitleText: 'WeChat',
-    navigationBarTextStyle: 'black'
-  }
-})
+    navigationBarTextStyle: 'black',
+    navigationStyle: 'custom',
+  },
+  rendererOptions: {
+    skyline: {
+      defaultDisplayBlock: true,
+      defaultContentBox: true,
+    },
+  },
+  renderer: 'skyline',
+  lazyCodeLoading: 'requiredComponents',
+  componentFramework: 'glass-easel',
+});
