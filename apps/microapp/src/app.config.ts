212c2b9a90bc4572d45754cb9d24a0b477715810
pidanmeng <1092333914@qq.com>
2024-06-23 22:51:29 +0800

    feat(microapp): add custom tab bar and achievement page
    - Implement a custom tab bar component with NutTabbar and NutTabbarItem.
    - Introduce the achievement page with navigation and button functionality.
    - Use hooks for navigation bar height and handle click events.
    - Update app configuration to include new pages and styles.
    - Refactor components and pages to improve user experience.

--- apps/microapp/src/app.config.ts
+++ apps/microapp/src/app.config.ts
@@ -1,5 +1,5 @@
 export default defineAppConfig({
-  pages: ['pages/index/index'],
+  pages: ['pages/index/index', 'pages/achievement/index', 'pages/my/index'],
   window: {
     navigationBarTextStyle: '@navTxtStyle',
     navigationBarBackgroundColor: '@navBgColor',
@@ -13,6 +13,32 @@ export default defineAppConfig({
       defaultDisplayBlock: true,
       defaultContentBox: true,
     },
+  },
+  tabBar: {
+    custom: true,
+    color: '@tabBarTxtColor',
+    selectedColor: '#DC143C',
+    backgroundColor: '@tabBarBgColor',
+    list: [
+      {
+        pagePath: 'pages/index/index',
+        selectedIconPath: 'images/gym-selected.png',
+        iconPath: 'images/gym.png',
+        text: '岩馆',
+      },
+      {
+        pagePath: 'pages/achievement/index',
+        selectedIconPath: 'images/achievement-selected.png',
+        iconPath: 'images/achievement.png',
+        text: '成就',
+      },
+      {
+        pagePath: 'pages/my/index',
+        selectedIconPath: 'images/my-selected.png',
+        iconPath: 'images/my.png',
+        text: '我的',
+      },
+    ],
   },
   lazyCodeLoading: 'requiredComponents',
   componentFramework: 'glass-easel',
